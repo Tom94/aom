67a82c750dc304830281bdf7e71e0825b79d21f8
Thomas Müller <git@tom94.net>
2025-10-20 11:39:26 +0200

    build: compatibility with nasm v3+

--- build/cmake/aom_optimization.cmake
+++ build/cmake/aom_optimization.cmake
@@ -215,10 +215,10 @@ function(test_nasm)
   execute_process(COMMAND ${CMAKE_ASM_NASM_COMPILER} -hf
                   OUTPUT_VARIABLE nasm_helptext)
 
-  if(NOT "${nasm_helptext}" MATCHES "-Ox")
-    message(
-      FATAL_ERROR "Unsupported nasm: multipass optimization not supported.")
-  endif()
+  # if(NOT "${nasm_helptext}" MATCHES "-Ox")
+  #   message(
+  #     FATAL_ERROR "Unsupported nasm: multipass optimization not supported.")
+  # endif()
 
   if("${AOM_TARGET_CPU}" STREQUAL "x86")
     if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
